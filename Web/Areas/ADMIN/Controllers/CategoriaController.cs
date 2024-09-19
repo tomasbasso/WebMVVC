e2d9125ed2a7d57ae6a10ea5f30eff290a30b422
tomasbasso <tomas.basso@hotmail.com>
2024-09-18 23:40:46 -0300

    Terminado categoria

--- Web/Areas/ADMIN/Controllers/CategoriaController.cs
+++ Web/Areas/ADMIN/Controllers/CategoriaController.cs
@@ -1,59 +1,47 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using APP.ACCESODATOS.Data.Repository.IRepository;
+﻿using APP.ACCESODATOS.Data.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.DotNet.Scaffolding.Shared.Messaging;
 using Categoria = APP.MODELO.Categoria;
 
-namespace PasajesApp.Areas.Admin.Controllers
+namespace APPnet8.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class CategoriaController : Controller
     {
+
         private readonly IContenedorTrabajo _contenedorTrabajo;
+
         public CategoriaController(IContenedorTrabajo contenedorTrabajo)
         {
             _contenedorTrabajo = contenedorTrabajo;
         }
-
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
 
-        
         [HttpGet]
-        public IActionResult GetAll()
+        public IActionResult Create()
         {
-            return Json(new { data = _contenedorTrabajo.Categoria.GetAll() });
+            return View();
         }
 
-        ///CREAR-----------------------------------------------------------
-
-
-        [HttpGet]
-        public IActionResult Create(int id)
-        {
-            Categoria categoria = new Categoria();
-            categoria = _contenedorTrabajo.Categoria.Get(id);
-            if (categoria == null)
-            {
-                return NotFound();
-            }
-            return View(categoria);
-        }
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Create (Categoria categoria)
+        public IActionResult Create(Categoria categoria)
         {
-            if (!ModelState.IsValid)
+            if (ModelState.IsValid)
             {
                 _contenedorTrabajo.Categoria.Add(categoria);
                 _contenedorTrabajo.Save();
                 return RedirectToAction("Index");
             }
+
             return View(categoria);
         }
 
-        ///EDITAR-----------------------------------------------------------
-        
+
         [HttpGet]
         public IActionResult Edit(int id)
         {
@@ -63,48 +51,48 @@ namespace PasajesApp.Areas.Admin.Controllers
             {
                 return NotFound();
             }
+
             return View(categoria);
         }
+
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Edit(Categoria categoria)
         {
-            if (!ModelState.IsValid)
+            if (ModelState.IsValid)
             {
                 _contenedorTrabajo.Categoria.Update(categoria);
                 _contenedorTrabajo.Save();
                 return RedirectToAction("Index");
             }
+
             return View(categoria);
         }
 
-
-        ///BORRAR-----------------------------------------------------------
-
+        #region Llamadas a la API
         [HttpGet]
+        public IActionResult GetAll()
+        {
+            var data = _contenedorTrabajo.Categoria.GetAll();
+            Console.WriteLine($"GetAll called. Returning {data.Count()} items.");
+            return Json(new { data = data });
+        }
+
+        [HttpDelete]
         public IActionResult Delete(int id)
         {
-            Categoria categoria = new Categoria();
-            categoria = _contenedorTrabajo.Categoria.Get(id);
-            if (categoria == null)
+            var objdesdebase = _contenedorTrabajo.Categoria.Get(id);
+            if (objdesdebase == null)
             {
-                return NotFound();
+                return Json(new { success = false, message = "Error borrando categoría" });
             }
-            return View(categoria);
-        }
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Delete(Categoria categoria)
-        {
-            if (!ModelState.IsValid)
-            {
-                _contenedorTrabajo.Categoria.Remove(categoria);
-                _contenedorTrabajo.Save();
-                return RedirectToAction("Index");
-            }
-            return View(categoria);
-        }
 
+            _contenedorTrabajo.Categoria.Remove(objdesdebase);
+            _contenedorTrabajo.Save();
+            return Json(new { success = true, message = "Categoría borrada correctamente" });
+        }
+        #endregion
 
     }
 }
--- Web/wwwroot/js/categoria.js
+++ Web/wwwroot/js/categoria.js
@@ -4,18 +4,67 @@ $(document).ready(function () {
     cargarDatatable();
 });
 
-
+function Delete(url) {
+    Swal.fire({
+        title: '¿Estás seguro?',
+        text: "¡No podrás revertir esto!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Sí, bórralo!',
+        cancelButtonText: 'Cancelar'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                type: "DELETE",
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        dataTable.ajax.reload();
+                        Swal.fire(
+                            'Borrado!',
+                            'La categoría ha sido eliminada.',
+                            'success'
+                        )
+                    } else {
+                        Swal.fire(
+                            'Error!',
+                            'Hubo un problema al borrar la categoría.',
+                            'error'
+                        )
+                    }
+                }
+            });
+        }
+    })
+}
 function cargarDatatable() {
+    if ($.fn.DataTable.isDataTable('#tblCategorias')) {
+        $('#tblCategorias').DataTable().destroy();
+    }
+
     dataTable = $("#tblCategorias").DataTable({
         "ajax": {
-            "url": "/admin/categoria/GetAll",
+            "url": "/Admin/Categoria/GetAll",
             "type": "GET",
-            "datatype": "json"
+            "datatype": "json",
+            "dataSrc": "data",
+            "error": function (xhr, error, thrown) {
+                console.error('Error en la llamada AJAX:', error);
+                console.error('Detalles:', xhr.responseText);
+            }
         },
         "columns": [
             { "data": "id", "width": "5%" },
             { "data": "nombre", "width": "40%" },
-            { "data": "habilitada", "width": "10%" },
+            {
+                "data": "habilitada",
+                "width": "10%",
+                "render": function (data) {
+                    return data ? "Sí" : "No";
+                }
+            },
             {
                 "data": "id",
                 "render": function (data) {
@@ -24,12 +73,12 @@ function cargarDatatable() {
                                 <i class="far fa-edit"></i> Editar
                                 </a>
                                 &nbsp;
-                                <a onclick=Delete("/Admin/Categoria/Delete/${data}") class="btn btn-danger text-white" style="cursor:pointer; width:140px;">
+                                <a onclick="Delete('/Admin/Categoria/Delete/${data}')" class="btn btn-danger text-white" style="cursor:pointer; width:140px;">
                                 <i class="far fa-trash-alt"></i> Borrar
                                 </a>
-                          </div>
-                         `;
-                }, "width": "40%"
+                            </div>`;
+                },
+                "width": "40%"
             }
         ],
         "language": {
